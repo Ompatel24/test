e3cc1b408c829f8858065c714b5ce51ab44ea68a
Ompatel24 <64767665+Ompatel24@users.noreply.github.com>
2021-03-15 21:44:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+/**
+ * * Q1.  30 Marks
+ * * Create a self executing function that returns an object describing an animal of your choice.
+ * * Ensure your animal object contains the following information
+ * * -weight
+ * * -covering
+ * * -length
+ * * -diet
+ * * -predators
+ * * -habitat
+ * * -source information web page
+ * 
+ * * Your object needs to contain at least:
+ * * -1 array containing 3 or more entries
+ * * -1 or more strings
+ * * -1 or more numbers
+ * * -1 or more booleans
+ */
+
+let rabbit = [
+    scientificName = "Oryctolagus cuniculus",
+    approxWeightInKg = 3,
+    covering = "rabbit fur",
+    minimumLengthInCm = 20,
+    diet = "omnivore",
+    predators = ["owls", "hawks", "eagles", "falcons", "wild dogs"],
+    food = ["Bell peppers", "Bok choy", "Brussels sprouts", "Carrot tops"],
+    maleRabbitIsDoe = false,
+    lifeSpanInYears = 9,
+    layEggs = false,
+    sourceReference = "https://www.livescience.com/28162-rabbits.html#:~:text=Rabbits%20are%20small%20mammals%20with,whiskers%20and%20distinctive%20long%20ears.&text=Rabbits%20and%20hares%20are%20in,Lepus%3B%20all%20others%20are%20rabbits.",
+];
+
+function()
+
+
+
+/**
+ * * Q2. 20 marks
+ * * Create a function expression that takes the output of question 1 and
+ * * loops backwards through the array in the object console logging each time. 
+ */
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * * Q3. 25 marks
+ * * Create a fat arrow function that takes in the output of question 1 and
+ * * returns a string in the following format: The __animalName__ lives in __habitat__ and has a covering of __covering__.
+ * * Ensure that your function is a single line.
+ */
+
+
+
+
+
+
+
+
+
+
+/**
+ * * Q4. 25 marks
+ * * Create a function declaration that takes in the output of question 1, and loops through
+ * * all of it's values, console logging each individual key:value pair.  
+ * * To answer this question review how to loop through an object.
+ */
